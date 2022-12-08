22fa161122005444df29025fea8bc8f3ff4af022
Farhan Kamil <104298474+Hanseee16@users.noreply.github.com>
2022-12-08 22:33:55 +0700

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,50 @@
+// Navbar aktif
+$(document).scroll(() => {
+  $(".navbar").toggleClass("active", $(this).scrollTop() > $(".navbar").height());
+});
+
+// Slide Image Home
+var swiper = new Swiper(".mySwiperHome", {
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+// Slide Product
+var swiper = new Swiper(".mySwiperProduct", {
+  slidesPerView: "auto",
+  spaceBetween: 40,
+  navigation: {
+    nextEl: ".bxs-chevron-right-circle",
+    prevEl: ".bxs-chevron-left-circle",
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
+
+// Slide Testimoni
+var swiper = new Swiper(".mySwiperTesti", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
